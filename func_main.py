daa203ec3ce328ac3e86ab172a8feaca1d428580
Yanghuiwon22 <hannah4212@naver.com>
2024-05-09 15:24:54 +0900

    streamlit start

---
+++ func_main.py
@@ -0,0 +1,46 @@
+import os
+import pandas as pd
+
+
+def set_data():
+    input_dir = './input'
+    output_dir = './output'
+
+    df = pd.DataFrame()
+
+    filelist = os.listdir(input_dir)
+    for i in filelist:
+        raw_data = pd.DataFrame(pd.read_excel(f'{input_dir}/{i}'))
+        df = pd.concat([df, raw_data])
+        df.reset_index(drop=True, inplace=True)
+
+    df.to_csv(f'{output_dir}/output.csv', index=False)
+
+def get_data():
+    df = pd.read_csv('./output/output.csv')
+    df_first = pd.DataFrame([])
+
+    grouped = df.groupby('사용기관명')
+
+    for group_name, group_df in grouped:
+        df_first = pd.concat([df_first,group_df])
+    df_first.reset_index(drop=True, inplace=True)
+
+    df_first.to_csv('./output/output_first.csv', index=False)
+
+def second_data():
+    df = pd.read_csv('./output/output_first.csv')
+
+    df = df.drop(['순번','건축물코드','건축물명','층구분','층코드','층번호','호실코드'], axis=1)
+    print(df.columns)
+    print(df.head())
+
+
+def main():
+    # set_data()
+    # get_data()
+    second_data()
+
+
+if __name__=='__main__':
+    main()
--- main.py
+++ main.py
@@ -1,34 +1,8 @@
-import os
-import pandas as pd
+import streamlit as st
+import func_main
+
+st.title('전북대 농업생명과학대 호실 사용 현황')
 
 
-def set_data():
-    input_dir = './input'
-    output_dir = './output'
-
-    df = pd.DataFrame()
-
-    filelist = os.listdir(input_dir)
-    for i in filelist:
-        raw_data = pd.DataFrame(pd.read_excel(f'{input_dir}/{i}'))
-        df = pd.concat([df, raw_data])
-        df.reset_index(drop=True, inplace=True)
-
-    df.to_csv(f'{output_dir}/output.csv', index=False)
-
-def get_data():
-    df = pd.read_csv('./output/output.csv')
-    df_first = pd.DataFrame()
-    grouped = df.groupby('사용기관명')
-    for group in grouped:
-        print(group)
-        # df_first = pd.concat([df_first,group])
-    # print(df_first)
-
-def main():
-    # set_data()
-    get_data()
-
-
-if __name__=='__main__':
-    main()
+st.sidebar.title('this is sidebar')
+st.sidebar.checkbox('체크박스에 표시될 문구')
