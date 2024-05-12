f3ba89811047498b23d6d828d8045fff5f422e41
Yanghuiwon22 <hannah4212@naver.com>
2024-05-12 23:03:01 +0900

    상세페이지-전체 생성

--- func_main.py
+++ func_main.py
@@ -25,17 +25,18 @@ def get_data():
 
     for group_name, group_df in grouped:
         df_first = pd.concat([df_first,group_df])
+        # group_nm.append(group_name.strip())
         group_nm.append(group_name)
+    print(group_nm)
     df_first.reset_index(drop=True, inplace=True)
 
 
     df_first.to_csv('./output/output_first.csv', index=False)
-    print(group_nm)
+    group_nm.insert(0,'전체')
     return group_nm
 
 def second_data():
     df = pd.read_csv('./output/output_first.csv')
-
     df = df.drop(['캠퍼스구분','순번','건축물코드','건축물명','층구분','층코드','층번호','호실코드'], axis=1)
     return df
 
--- main.py
+++ main.py
@@ -1,22 +1,26 @@
 import streamlit as st
 import func_main
 
+# 메인 페이지
 st.title('전북대 농업생명과학대 호실 사용 현황')
 
 
-st.sidebar.title('사업체별 확인')
-
-# select_species 변수에 사용자가 선택한 값이 지정됩니다
+# 사이드바
 select_company = st.sidebar.selectbox(
     '확인하고 싶은 기관을 선택하세요',
     func_main.get_data()
 )
 
+# 상세 페이지
 df = func_main.second_data()
 tmp_df = df[df['사용기관명'] == select_company]
 
-st.write(select_company)
-st.dataframe(tmp_df)
+## 상세페이지 - 전체
+if select_company == '전체':
+    st.dataframe(df)
+else:
+    st.write(select_company)
+    st.dataframe(tmp_df)
 
 
 
