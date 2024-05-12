0353c1d58069a430f2d9981969f4154aeb32972f
Yanghuiwon22 <hannah4212@naver.com>
2024-05-12 23:58:41 +0900

    사용기관별 전용면적 그래프 생성.

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import streamlit as st
 import func_main
 
-# 메인 페이지
-st.title('전북대 농업생명과학대 호실 사용 현황')
-
 
 # 사이드바
 select_company = st.sidebar.selectbox(
@@ -11,17 +8,30 @@ select_company = st.sidebar.selectbox(
     func_main.get_data()
 )
 
+# 메인 페이지
+if select_company == '전체':
+    spot = '전북대 농업생명과학대학'
+else:
+    spot = select_company
+st.title(f'{spot} 호실 사용 현황')
+
 # 상세 페이지
 df = func_main.second_data()
 tmp_df = df[df['사용기관명'] == select_company]
 
+
 ## 상세페이지 - 전체
 if select_company == '전체':
+    st.bar_chart(data=df, x='사용기관명', y='전용면적',) # color=None, width=0, height=0, use_container_width=True)
+
     st.dataframe(df)
+
 else:
-    st.write(select_company)
+    st.bar_chart(data=tmp_df, x='호실명', y='전용면적',) # color=None, width=0, height=0, use_container_width=True)
+
     st.dataframe(tmp_df)
 
+#test
 
 
 
