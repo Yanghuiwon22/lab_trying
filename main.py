3270391977a5982c9d26374bf1e43234e5171489
Yanghuiwon22 <hannah4212@naver.com>
2024-05-13 12:00:19 +0900

    도커에 올리기

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import func_main
+import altair as alt
 
 
 # 사이드바
@@ -22,7 +23,14 @@ tmp_df = df[df['사용기관명'] == select_company]
 
 ## 상세페이지 - 전체
 if select_company == '전체':
-    st.bar_chart(data=df, x='사용기관명', y='전용면적',) # color=None, width=0, height=0, use_container_width=True)
+
+    chart = alt.Chart(df).mark_bar().encode(
+        x='전용면적',
+        y=alt.Y('사용기관명', axis=alt.Axis(title='사용기관명', labelFontSize=14))  # y 축의 라벨 크기를 조절합니다.
+    ).properties(
+        height=alt.Step(30)  # 그래프의 높이를 조절합니다.
+    )
+    st.altair_chart(chart, use_container_width=True)
 
     st.dataframe(df)
 
